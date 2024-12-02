6120597f108cc97425764f7c1e024563003bbca6
Noa Katz <noakatz465@gmail.com>
2024-12-02 14:57:14 +0200

    correct schemas

--- src/app/types/taskInterface.ts
+++ src/app/types/taskInterface.ts
@@ -1,3 +1,16 @@
+import mongoose, { Document } from 'mongoose';
+
 export interface ITask extends Document {
-    
+    title: string;
+    description?: string;
+    dueDate?: Date;
+    frequency?: string;
+    status: string;
+    priority?: string;
+    creator: mongoose.Types.ObjectId;
+    assignedUsers?: mongoose.Types.ObjectId[];
+    reminderDateTime?: Date;
+    templateId?: mongoose.Types.ObjectId;
+    lastModified: Date;
+    projectId?: mongoose.Types.ObjectId;
 }
--- src/app/types/userInterface.ts
+++ src/app/types/userInterface.ts
@@ -1,14 +1,15 @@
 import mongoose, { Document } from 'mongoose';
 
 export interface IUser extends Document {
-    name: string;
-    birthDate: Date;
+    firstName: string;
+    lastName: string;
+    birthDate?: Date;
     email: string;
-    gender: string;
+    gender?: string;
     password: string;
     joinDate: Date;
     notificationsEnabled: boolean;
-    projects: mongoose.Types.ObjectId[];
-    tasks: mongoose.Types.ObjectId[];
-    sharedWith: mongoose.Types.ObjectId[];
+    projects?: mongoose.Types.ObjectId[];
+    tasks?: mongoose.Types.ObjectId[];
+    sharedWith?: mongoose.Types.ObjectId[];
   }
