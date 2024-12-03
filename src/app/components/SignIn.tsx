f418a530f2fd3a679fae193ac256c26c8ad10869
Gitty Sinai <gitty.sinai214@gmail.com>
2024-12-03 14:08:50 +0200

    add basic login component

--- src/app/components/SignIn.tsx
+++ src/app/components/SignIn.tsx
@@ -1,11 +1,105 @@
-import React from 'react'
+"use client";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
 
 function SignIn() {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [gender, setGender] = useState(""); // 'M' or 'F'
+  const [birthDate, setBirthDate] = useState(""); // DATE
+  const [error, setError] = useState("");
+
+  const router= useRouter()
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    if ( !firstName || !lastName || !email || !password) {
+      setError("ID, First Name, Last Name, Email, and Password are required.");
+      return;
+    }
+    setError(""); 
+    console.log({
+      firstName,
+      lastName,
+      email,
+      password,
+      gender,
+      birthDate,
+    });
+  
+  };
+  
+
   return (
-    <div>
-      
+    <div className="grid place-items-center h-screen bg-gray-50">
+      <div className="shadow-lg p-6 rounded-lg border-t-4 border-green-400 max-w-md w-full bg-white">
+        <h1 className="text-xl font-bold mb-4 text-center">Sign In</h1>
+        <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+        <input
+            type="text"
+            placeholder="First Name"
+            className="border border-gray-300 rounded-md py-2 px-4 focus:outline-none focus:ring-2 focus:ring-green-400"
+            value={firstName}
+            onChange={(e) => setFirstName(e.target.value)}
+          />
+          <input
+            type="text"
+            placeholder="Last Name"
+            className="border border-gray-300 rounded-md py-2 px-4 focus:outline-none focus:ring-2 focus:ring-green-400"
+            value={lastName}
+            onChange={(e) => setLastName(e.target.value)}
+          />
+          <input
+            type="email"
+            placeholder="Email"
+            className="border border-gray-300 rounded-md py-2 px-4 focus:outline-none focus:ring-2 focus:ring-green-400"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            className="border border-gray-300 rounded-md py-2 px-4 focus:outline-none focus:ring-2 focus:ring-green-400"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+           <select
+            className="border border-gray-300 rounded-md py-2 px-4 focus:outline-none focus:ring-2 focus:ring-green-400"
+            value={gender}
+            onChange={(e) => setGender(e.target.value)}
+          >
+            <option value="">Select Gender</option>
+            <option value="M">Male</option>
+            <option value="F">Female</option>
+          </select>
+          <input
+            type="date"
+            className="border border-gray-300 rounded-md py-2 px-4 focus:outline-none focus:ring-2 focus:ring-green-400"
+            value={birthDate}
+            onChange={(e) => setBirthDate(e.target.value)}
+          />
+          <button
+            type="submit"
+            className="bg-green-600 text-white font-bold py-2 rounded-md hover:bg-green-700 transition duration-200"
+          >
+            Sign In
+          </button>
+          {error && (
+            <div className="bg-red-500 text-white w-fit text-sm py-1 px-3 rounded-md mt-2">
+              {error}
+            </div>
+          )}
+          <div className="text-right mt-4">
+            <Link className="text-sm text-blue-500 hover:underline" href="/">
+              Already have an account? <span className="underline">Login</span>
+            </Link>
+          </div>
+        </form>
+      </div>
     </div>
-  )
+  );
 }
 
-export default SignIn
+export default SignIn;
+
