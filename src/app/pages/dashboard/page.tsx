678f82204547807af749e94f9639bcbc737b4960
Gitty Sinai <gitty.sinai214@gmail.com>
2024-12-03 12:26:21 +0200

    Adding a basic structure of folders and pages for login and registration

---
+++ src/app/pages/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import UserInfo from '@/app/components/UserInfo'
+import React from 'react'
+
+function page() {
+  return (
+    <div>
+        <UserInfo/>
+    </div>
+  )
+}
+
+export default page
---
+++ src/app/pages/signIn/page.tsx
@@ -0,0 +1,12 @@
+import SignIn from '@/app/components/SignIn'
+import React from 'react'
+
+function page() {
+  return (
+    <div>
+     <SignIn/> 
+    </div>
+  )
+}
+
+export default page
